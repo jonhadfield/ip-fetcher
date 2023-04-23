b3552c9732b70cee03b2c43f2bdf64f20a9fa239
Jon Hadfield <jon@lessknown.co.uk>
2023-04-23 11:30:10 +0100

    add cloudflare.

---
+++ cloudflare/cloudflare_test.go
@@ -0,0 +1,30 @@
+package cloudflare
+
+import (
+	"fmt"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestFetch(t *testing.T) {
+	u, err := url.Parse(DownloadURL)
+	require.NoError(t, err)
+
+	urlBase := fmt.Sprintf("%s://%s", u.Scheme, u.Host)
+
+	gock.New(urlBase).
+		Get(u.Path).
+		Reply(200).
+		// SetHeader("Etag", "cd5e4f079775994d8e49f63ae9a84065").
+		File("testdata/cloud.json")
+
+	ac := New()
+	gock.InterceptClient(ac.Client.HTTPClient)
+
+	doc, err := ac.Fetch()
+	require.NoError(t, err)
+	require.NotEmpty(t, doc.IPv4Prefixes)
+}
